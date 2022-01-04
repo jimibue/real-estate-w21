79f5f05c9233bd011a301718e9d542b6bd2de620
James Yeates <jamesyeates@Jamess-MacBook-Pro-2.local>
2022-01-04 11:36:20 -0800

    av be done

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,23 @@
 class Property < ApplicationRecord
   belongs_to :agent
   has_one :address
+
+  # SELECT properties.id, price, beds,  baths, ad.city, ad.zip, 
+  # ad.street, a.id as agent_id, a.first_name, a.last_name, a.email, 
+  # ad.id as address_id
+  # FROM properties
+  # INNER JOIN agents AS a ON a.id = properties.agent_id
+  # INNER JOIN addresses AS ad ON ad.property_id = properties.id
+  # WHERE properties.sold <> true
+  # ORDER BY a.id;
+
+  def self.available
+    select('properties.id, price, beds,  baths, ad.city, ad.zip, 
+    ad.street, a.id as agent_id, a.first_name, a.last_name, a.email, 
+    ad.id as address_id')
+    .joins('INNER JOIN agents AS a ON a.id = properties.agent_id
+    INNER JOIN addresses AS ad ON ad.property_id = properties.id')
+    .where('properties.sold <> true')
+    .order('a.id')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :cats, only: [:index, :update]
+    get "properties", to: "properties#index"
   end
 end
