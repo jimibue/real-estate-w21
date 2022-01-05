f2200d84570adf32ab38673537ad4278ba13f945
James Yeates <jamesyeates@Jamess-MacBook-Pro-2.local>
2022-01-05 09:56:03 -0800

    get agent data

--- client/src/pages/Available.js
+++ client/src/pages/Available.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState, useEffect,} from 'react'
 
 const Available = (props)=>{
+
+	const [agentProperties, setAgentProperties] = useState([])
+	useEffect(()=>{
+		getAgentProperties()
+	},[])
+
+	const getAgentProperties = async ()=> {
+    try {
+			let res = await axios.get('/api/properties')
+			console.log(res)
+			setAgentProperties(res.data)
+		} catch (error) {
+			alert('errror occured get agentProperties')
+		}
+	}
   return (
 		<div>
 			<h1>Available Page</h1>
