29307c30f8b36136e86b99e5158606a12f04b4fa
James Yeates <jamesyeates@Jamess-MacBook-Pro-2.local>
2022-01-06 10:49:39 -0800

    hooked up to backend cities featues

--- client/src/pages/Cities.js
+++ client/src/pages/Cities.js
@@ -4,15 +4,9 @@ import React, { useState, useEffect } from 'react'
 import PropertyCard from '../components/PropertyCard';
 const { Option } = Select;
 
-const dummyCityProperties = [
-  { sq_ft: 1234, beds: 3, baths: 2, price: 312345 },
-  { sq_ft: 234, beds: 3, baths: 2, price: 13345 },
-  { sq_ft: 3343, beds: 2, baths: 2, price: 23345 },
-]
 
 const Cities = () => {
   const [cities, setCities] = useState(null)
-  const [city, setCity] = useState(null)
   const [cityProperties, setCityProperties] = useState(null)
   useEffect(() => {
     getCities()
@@ -23,19 +17,18 @@ const Cities = () => {
       let res = await axios.get('/api/cities')
       setCities(res.data)
     } catch (err) {
+      alert('err in getCities')
       // API not setup so use dummy data
-      setCities(["SLC", "Draper", "Provo"])
+    
     }
   }
   const handleChange = async (value) => {
-    console.log(`selected ${value}`);
     // api call to get city data
     try {
-      let res = await axios.get(`/api/city/${value.toLowerCase()}`)
-      setCity(value)
+      let res = await axios.get(`/api/cities/${value}`)
       setCityProperties(res.data)
     } catch (err) {
-      setCityProperties(dummyCityProperties)
+      alert('err in handleChange')
       console.log(err)
     }
   }
@@ -55,7 +48,6 @@ const Cities = () => {
   }
 
   const renderCity = () => {
-    console.log('cityProperties: ', cityProperties)
     if (!cityProperties) {
       return <p>Loading cities / or select a city</p>
     }
@@ -64,7 +56,7 @@ const Cities = () => {
         <Row>
           {cityProperties.map(cp => {
             return (
-              <Col sm={24} md={12} lg={8}>
+              <Col key={cp.id} sm={24} md={12} lg={8}>
                 <PropertyCard {...cp} />
               </Col>
             )
